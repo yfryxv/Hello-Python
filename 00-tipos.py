4b3a3cd27fcbca96d6a8500b02a67bfc67d8862e
yefryxv <arapayefry1@gmail.com>
2025-06-07 14:04:28 -0500

    3

--- 00-tipos.py
+++ 00-tipos.py
@@ -4,3 +4,5 @@ print("Hello word") #-> String
 #Con esto vemos el tipo de dato de las variables
 print(type("Hello word")) #-> Type String
 print(type(1)) #-> Type Integer
+print(type(1.0)) #-> Type Float
+print(type(True)) #-> Type Boolean
---
+++ 01-variables.py
@@ -0,0 +1,4 @@
+#Variables
+my_variable = "esto es una variable"
+#Imprimir en consola el valor de la variable
+print(my_variable) #-> esto es una variable
