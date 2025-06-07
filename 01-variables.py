2f7c20414cdc8ce2a47a3d387ffc75e4b6e0ec95
yefryxv <arapayefry1@gmail.com>
2025-06-07 16:13:58 -0500

    4

--- 01-variables.py
+++ 01-variables.py
@@ -2,3 +2,18 @@
 my_variable = "esto es una variable"
 #Imprimir en consola el valor de la variable
 print(my_variable) #-> esto es una variable
+
+my_int_variable = 10
+print(my_int_variable) #-> 10
+
+my_bool_variable = True
+print(my_bool_variable) #-> True
+
+#Concatenamos variables
+print(my_variable, my_int_variable, my_bool_variable) #->Concatena los valores de las variables
+
+
+#cambiamos rl tipoo de las variables
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable)) #-> Type Integer
